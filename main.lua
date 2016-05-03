d38c95b2210fc7365c3b27bbe5a7bf3a0cf311a8
NoisyBass <laura.dcs4@gmail.com>
2016-05-03 20:55:53 +0200

    Proving love2d physics

--- main.lua
+++ main.lua
@@ -3,7 +3,38 @@ local anim8 = require 'anim8'
 local x, y = 400, 300
 
 function love.load()
+
+	-- One meter is 64px in physics engine
+	love.physics.setMeter(64)
+
+	-- Create a world with standard gravity
+	world = love.physics.newWorld(0, 9.81*64, true)
+
+	-- Create the ground body at (0, 0) static
+	ground = love.physics.newBody(world, 0, 0, "static")
+	
+	-- Create the ground shape at (400,500) with size (600,10).
+	ground_shape = love.physics.newRectangleShape( 400, 500, 600, 10)
+
+	-- Create fixture between body and shape
+	ground_fixture = love.physics.newFixture( ground, ground_shape)
+
 	image = love.graphics.newImage('media/trex_run.png')
+
+	-- Create a Body for the circle
+	body = love.physics.newBody(world, 400, 200, "dynamic")
+	
+	-- Attatch a shape to the body.
+	circle_shape = love.physics.newRectangleShape( 0, 0, 128, 128)
+	
+    -- Create fixture between body and shape
+    fixture = love.physics.newFixture( body, circle_shape)
+
+    -- Calculate the mass of the body based on attatched shapes.
+	-- This gives realistic simulations.
+	body:setMassData(circle_shape:computeMass( 1 ))
+
+
 
 	local grid = anim8.newGrid(128, 128, 256, 128)
 
@@ -11,6 +42,10 @@ function love.load()
 end
 
 function love.update(dt)
+
+	-- Update the world.
+	world:update(dt)
+
 	if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
 		x = x - 100 * dt
 	end
@@ -23,6 +58,10 @@ function love.update(dt)
 	if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
 		y = y + 100 * dt
 	end
+	if love.keyboard.isDown("space") then
+		-- Apply a random impulse
+		body:applyLinearImpulse(0,-10)
+	end
 
 	--if love.mouse.isDown(1) then
 	--	animation: pause()
@@ -34,11 +73,24 @@ function love.update(dt)
 end
 
 function love.draw()
-	animation: draw(image, x, y)
+
+	-- Draws the ground.
+	love.graphics.polygon("line", ground:getWorldPoints(ground_shape:getPoints()))
+
+	--love.graphics.draw(image,body:getX(), body:getY(), body:getAngle(),1,1,64,64)
+
+	--animation: draw(image, x, y)
+	animation: draw(image, body:getX() - 64, body:getY() - 64)
 end
 
 function love.mousepressed(x, y, button, istouch)
 	if button == 1 then
 		animation: flipH()
 	end
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+       love.event.quit()
+    end
 end
